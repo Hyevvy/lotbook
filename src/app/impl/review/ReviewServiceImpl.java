780e6023e8bb7d294c3d4d62eecde582fc342441
CessnaJ <eon8748@gmail.com>
2023-08-18 03:02:30 +0900

    ✈ [ADD] ReviewServiceImpl 추가

---
+++ src/app/impl/review/ReviewServiceImpl.java
@@ -0,0 +1,40 @@
+package app.impl.review;
+
+import java.util.List;
+
+import app.dto.entity.Review;
+import app.frame.ServiceFrame;
+
+public class ReviewServiceImpl implements ServiceFrame<Review, Review>{
+
+	@Override
+	public int register(Review v) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int modify(Review v) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int remove(Review k) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Review get(Review k) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Review> get() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

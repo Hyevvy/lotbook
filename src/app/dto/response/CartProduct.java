62254c4dc5ef836a3f05fbaaa1ea8e286e778b7b
kkh <rlghks131@korea.ac.kr>
2023-08-21 09:53:22 +0900

    🍺 [TEST] : 머지 테스트

---
+++ src/app/dto/response/CartProduct.java
@@ -0,0 +1,35 @@
+package app.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartProduct {
+	private long sequence;
+	private long productSequence;
+	private long memberSequence;
+	private boolean isDeleted;
+	private int count;
+	
+	private String productImgurl;
+	private String name;
+	private int price;
+	private String content;
+	private int stock;
+	private String createdAt;
+	private String updatedAt;
+	private double pointAccumulationRate;
+	private double discountRate;
+	private long salesCount;
+	private String state;
+	
+	// FK
+	private int authorSequence;
+	private int publisherSequence;
+	private int categorySequence;
+}
--- webapp/main.jsp
+++ webapp/main.jsp
@@ -29,7 +29,7 @@
 				<ul>
 					<c:choose>
 						<c:when test="${logincust != null }">
-							<li class="active"><a href="main.bit?view=mypage"><i
+							<li class="active"><a href="main.bit?view=mypage&memberSeq=${logincust.sequence }"><i
 									class="fa fa-user"></i> 마이페이지</a></li>
 							<li class=""><a href="member.bit?view=logout"><i
 									class="fa fa-user"></i> 로그아웃</a></li>
